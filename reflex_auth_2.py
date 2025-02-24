4302806197ab4ebcf723b8ff734f0ee1708f61aa
jawad <jkazi@sparkcognition.com>
2025-02-24 11:58:04 -0600

    a

--- reflex_auth_2.py
+++ reflex_auth_2.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 from authlib.integrations.starlette_client import OAuth
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
+from starlette.routing import Route
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 from urllib.parse import urlencode
-import json
 
 load_dotenv()
 
@@ -22,21 +23,27 @@ oauth.register(
     ),
 )
 
+def index():
+    return rx.text("Hello there! You are logged in.")
 
 async def login_endpoint(request: Request):
     callback_uri = request.url_for("callback_endpoint")
     return await oauth.keycloak.authorize_redirect(request, callback_uri)
 
 async def callback_endpoint(request: Request):
+    if "user_token" not in request.session:
+        return RedirectResponse("/login")
+
     token = await oauth.keycloak.authorize_access_token(request)
-    token_json = json.dumps(dict(token)) #
-    response = RedirectResponse(url="http://172.31.0.153:3000/") 
-    return response
+    request.session["user_token"] = token["access_token"]
+    return RedirectResponse(url="http://172.31.0.153:3000/")
 
 async def logout_endpoint(request: Request):
+    request.session.clear()
+    login_url = request.url_for("login_endpoint")
     params = {
         "client_id": os.environ.get("KEYCLOAK_CLIENT_ID"),
-        "post_logout_redirect_uri": request.url_for("login_endpoint"),
+        "post_logout_redirect_uri": login_url,
     }
     logout_url = (
         f'http://{os.environ.get("KEYCLOAK_DOMAIN")}/realms/'
@@ -45,34 +52,36 @@ async def logout_endpoint(request: Request):
     )
     return RedirectResponse(url=logout_url)
 
-async def redirect_main():
-    print(State.set_false())
-    print('hi')
-    return RedirectResponse(url="http://172.31.0.153:3000/")
-    
-class State(rx.State):
-    is_auth: bool = True
-    token: str = '' # 
-    
-def index() -> rx.Component:
-    return rx.cond(
-                State.is_auth,
-                page(),
-                rx.link("Login via Keycloak", href="/login"),
-    )
 
-def page() -> rx.Component:
-    return rx.text(State.token) #
+def page():
+    return rx.cond(
+            AuthState.check_auth,
+            rx.text("logged in..."),
+            rx.text("not logged in :(..."),
+        )
+
+async def auth_check(request: Request, next: str = "http://localhost:3000/"):
+    if "user_token" not in request.session:
+        return RedirectResponse('/login')
+    else:
+        return RedirectResponse(url=next)
+
+class AuthState(rx.State):
+    def check_auth(self):
+        # next_url = "http://localhost:3000/protected"
+        # q = urlencode({"next": next_url})
+        print('working')
+        # return rx.redirect(f"http://localhost:8000/auth-check?{q}")
+        return True
+
 
 app = rx.App()
-app.add_page(index)
-
 app.api.add_middleware(
-    SessionMiddleware,
-    secret_key=os.environ.get("APP_SECRET_KEY", "mydefaultsecret")
+    SessionMiddleware, secret_key=os.environ.get("APP_SECRET_KEY", "mydefaultsecret")
 )
-
-app.api.add_api_route("/login", login_endpoint, methods=["GET"])
-app.api.add_api_route("/callback", callback_endpoint, methods=["GET"])
+app.add_page(index) 
+app.add_page(page)
+app.api.add_route("/login", login_endpoint, methods=["GET"])
+app.api.add_route("/callback", callback_endpoint, methods=["GET"])
 app.api.add_api_route("/logout", logout_endpoint, methods=["GET"])
-app.api.add_api_route("/", redirect_main, methods=["GET"])
+app.api.add_api_route('/auth-check', auth_check, methods=['GET'])
